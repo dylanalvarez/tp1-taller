0b1c498ebec809b094c88bd025baeef33d16d6a3
Dylan Alvarez <helllo5d123@gmail.com>
2017-08-26 13:16:32 -0300

    reduce code repetition in splitRopeNode

--- rope/node.c
+++ rope/node.c
@@ -31,13 +31,13 @@ bool isLeaf(RopeNode *self) {
 void
 splitRopeNode(RopeNode *source, size_t index, RopeNode *left, RopeNode *right) {
     size_t string_length = strlen(source->content);
-    char left_string[index + 1];
-    memcpy(left_string, source->content, index);
-    left_string[index] = 0;
     char right_string[string_length - index + 1];
     memcpy(right_string, source->content + index, string_length - index);
     right_string[string_length - index] = 0;
-    createRopeNode(left, left_string);
+
+    _createEmptyRopeNode(left);
+    _setRopeNodeContent(left, source->content, 0, index);
+
     createRopeNode(right, right_string);
     source->left = left;
     source->right = right;
